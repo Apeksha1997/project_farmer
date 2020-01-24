39838fc967ccbd29660727788a6fdd7d14a1b12e
Apeksha1997 <32430370+Apeksha1997@users.noreply.github.com>
2020-01-24 16:09:55 +0530

    Update Main.java

--- Farmer1/src/main/java/com/lti/main/Main.java
+++ Farmer1/src/main/java/com/lti/main/Main.java
@@ -1,10 +1,13 @@
-package com.lti.main;
+package com.lti;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
-	@SpringBootApplication
 
 	public static void main(String[] args) {
-	
+		SpringApplication.run(Main.class, args);
 	}
-
 }
+
